aa095e12a7786b8564f9e9b397e63c09a3255715
Aditee Jha <aditeejha2005@gmail.com>
2026-02-12 19:16:17 +0530

    changes

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    const searchButton = document.getElementById("search-btn");
+    const usernameInput = document.getElementById("user-input");
+    const statsContainer = document.querySelector(".stats-container");
+    const easyprogressCircle = document.querySelector(".easy-progress");
+    const mediumProgressCircle = document.querySelector(".medium-progress");
+    const hardProgressCircle = document.querySelector(".hard-progress");
+    const easylabel = document.getElementById("easy-label");
+    const mediumLabel = document.getElementById("medium-label");
+    const hardLabel = document.getElementById("hard-label");z
+    const cardStatsContainer = document.querySelector(".stats-cards");
+})

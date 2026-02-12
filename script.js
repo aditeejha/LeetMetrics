7e07eab26e443d5aaba2de820f0d32935b60e7c5
Aditee Jha <aditeejha2005@gmail.com>
2026-02-12 19:38:27 +0530

    changes

--- script.js
+++ script.js
@@ -10,4 +10,27 @@ document.addEventListener("DOMContentLoaded", function(){
     const mediumLabel = document.getElementById("medium-label");
     const hardLabel = document.getElementById("hard-label");z
     const cardStatsContainer = document.querySelector(".stats-cards");
+
+    //return true or false based on regex(regular expression)
+    function validateUsername(username) {
+        if(username.trim()===""){
+            alert("Username cannot be empty");
+            return false;
+        }
+        const regex= /^[a-zA-Z0-9_-]$/;
+        const isMatching = regex.test(username);
+        if(!isMatching){
+            alert("Invalid Username");
+        }
+        return isMatching;
+    }
+
+    
+
+    searchButton.addEventListener('click', function(){
+        const username = usernameInput.value;
+        console.log("logging username: ", username);
+        if(validateUsername(username)){
+        }
+    })
 })
